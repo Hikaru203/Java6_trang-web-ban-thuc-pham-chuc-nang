11916d8e2acc836b8c4f8b15e338a79d6f20c539
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-07-30 13:48:26 +0700

    cart

--- asmjava6/src/main/java/com/poly/controller/AdminController.java
+++ asmjava6/src/main/java/com/poly/controller/AdminController.java
@@ -84,4 +84,5 @@ public class AdminController {
     public String ReportRevenue() {
         return "admin/ReportRevenue";
     }
+    
 }
--- asmjava6/src/main/java/com/poly/controller/HomeController.java
+++ asmjava6/src/main/java/com/poly/controller/HomeController.java
@@ -1,19 +1,22 @@
 package com.poly.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @Controller
 public class HomeController {
 	@GetMapping("/index")
 	public String index() {
-	    return "index";
+		return "index";
 	}
+
 	@GetMapping("/shop")
 	public String shop() {
-	    return "shop";
+		return "shop";
+	}
+
+	@GetMapping("/cart")
+	public String cart() {
+		return "cart";
 	}
 }
