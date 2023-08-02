34b4ffbcf26bcfdf5ad876545114bf8abbf49237
VuongTanPhat <133354234+VuongTanPhat@users.noreply.github.com>
2023-08-02 23:47:33 +0700

    Phân Loại Sản Phẩm

--- asmjava6/src/main/java/com/example/entity/Category.java
+++ asmjava6/src/main/java/com/example/entity/Category.java
@@ -8,10 +8,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 @Entity
 @Table(name = "Categories")
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
