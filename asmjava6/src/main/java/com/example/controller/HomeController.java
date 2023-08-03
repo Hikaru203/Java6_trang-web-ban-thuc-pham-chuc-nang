2e9ff311e641626a8f66a83e0d8fa272244f96a5
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-08-03 15:09:50 +0700

    Login

--- asmjava6/src/main/java/com/example/controller/HomeController.java
+++ asmjava6/src/main/java/com/example/controller/HomeController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.entity.Product;
+import com.example.entity.User;
 import com.example.jparepository.ProductRepository;
+import com.example.jparepository.UserRepository;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @Controller
@@ -17,11 +19,26 @@ public class HomeController {
 	@Autowired
 	ProductRepository daoProduct;
 
+	@Autowired
+	UserRepository userRepository;
+
 	@RequestMapping(value = "/index")
 	public String index(Model model) {
 		List<Product> page = daoProduct.findAll();
 		model.addAttribute("products", page);
 		return "index";
+	}
+
+	@GetMapping("/login")
+	public String showLoginFrom(Model model) {
+		model.addAttribute("user", new User());
+		return "login";
+	}
+
+	@GetMapping("/sinup")
+	public String showsinupFrom(Model model) {
+		model.addAttribute("user", new User());
+		return "dangky";
 	}
 
 }
