3b9a205293649385e2abf6220a849cfb655d03b1
VuongTanPhat <133354234+VuongTanPhat@users.noreply.github.com>
2023-08-05 07:59:33 +0700

    Update Product.java

--- asmjava6/src/main/java/com/example/entity/Product.java
+++ asmjava6/src/main/java/com/example/entity/Product.java
@@ -17,8 +17,8 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.DecimalMin;
 
 @Entity
 @Table(name = "Products")
