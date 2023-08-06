8babce252c39f2db7fb7bd04acdf5b04ecbaace7
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-08-06 11:14:13 +0700

    306

---
+++ asmjava6/src/main/java/com/example/controller/SecurityController.java
@@ -0,0 +1,7 @@
+package com.example.controller;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityController extends WebSecurityConfigurerAdapter{
+    
+}
