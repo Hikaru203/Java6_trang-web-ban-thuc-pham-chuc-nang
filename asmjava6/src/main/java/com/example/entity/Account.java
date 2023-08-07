d99cf86abeb92bc6841e753792997ccc149c8d12
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-08-07 11:09:15 +0700

    Squashed commit of the following:
    commit 7fc4dab039361c1c5aa13997f3bba887225ee73e
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Sun Aug 6 22:33:55 2023 +0700
    
        Login thành công
    
    commit 8babce252c39f2db7fb7bd04acdf5b04ecbaace7
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Sun Aug 6 11:14:13 2023 +0700
    
        306
    
    commit 16891129caee490e1ef552bc7fd00ffc3d23e41a
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Sat Aug 5 19:06:10 2023 +0700
    
        đổi phiên bản
    
    commit 9e94294ab9c1b7ad2c66878acdae0c3c957c2b54
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Sat Aug 5 10:39:47 2023 +0700
    
        login
    
    commit 83a992988a65da8d67b4fb40f40b8626e41e1781
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Thu Aug 3 18:08:08 2023 +0700
    
        update
    
    commit 7c543bfdcfe436480a42f05d7a93e25a76d9ae20
    Merge: 1da8c57 12255e0
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Thu Aug 3 16:58:10 2023 +0700
    
        Merge remote-tracking branch 'origin/Huy' into Phương
    
    commit 1da8c57c08dc6c93bdd0050594ba0864d6b93d47
    Merge: 2e9ff31 73ace02
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Thu Aug 3 15:11:38 2023 +0700
    
        Merge remote-tracking branch 'origin/Dev' into Phương
    
    commit 2e9ff311e641626a8f66a83e0d8fa272244f96a5
    Author: Phuongpiper <nguyenhongphuong.vn1@gmail.com>
    Date:   Thu Aug 3 15:09:50 2023 +0700
    
        Login

--- asmjava6/src/main/java/com/example/entity/Account.java
+++ asmjava6/src/main/java/com/example/entity/Account.java
@@ -3,6 +3,8 @@ package com.example.entity;
 import java.io.Serializable;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
