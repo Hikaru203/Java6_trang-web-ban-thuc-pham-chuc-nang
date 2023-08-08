ae832113a9e4afffaf53bf05e9551ab562322d09
Phuongpiper <nguyenhongphuong.vn1@gmail.com>
2023-08-08 23:10:43 +0700

    Update HomeController.java

--- asmjava6/src/main/java/com/example/controller/HomeController.java
+++ asmjava6/src/main/java/com/example/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.controller;
 
-
 import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +27,6 @@ import com.example.service.CookieService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Controller
 public class HomeController {
 	@Autowired
@@ -37,13 +35,12 @@ public class HomeController {
 	AccountRepository daoAccount;
 	@Autowired
 	private CookieService cookieService;
+
 	@RequestMapping("/client/login")
 	public String lgoin(Model model) {
 		model.addAttribute("user", new Account());
 		return "login";
 	}
-	
-	
 
 	@RequestMapping("/client/signin")
 	public String showsinupFrom(Model model) {
@@ -62,8 +59,8 @@ public class HomeController {
 			List<Product> page = daoProduct.findAll();
 			model.addAttribute("products", page);
 		}
-//		List<Cart> cartItem=cartService.findAll();
-//		model.addAttribute("cartItem",cartItem);
+		// List<Cart> cartItem=cartService.findAll();
+		// model.addAttribute("cartItem",cartItem);
 		return "index";
 	}
 
@@ -74,38 +71,39 @@ public class HomeController {
 		return "detail";
 	}
 
-	   @RequestMapping(value = "/client/login/success")
-	    public String success(Model model, HttpServletResponse response) {
-	        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-	            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+	@RequestMapping(value = "/client/login/success")
+	public String success(Model model, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+			UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-	            String username = userDetails.getUsername();
+			String username = userDetails.getUsername();
 
-	            Account account = daoAccount.findByUserName(username);
+			Account account = daoAccount.findByUserName(username);
 
-	            if (account != null) {
-                String cleanedUsername = account.getUserName().replaceAll("\\s", "");
-                cookieService.setCookie(response, "username", cleanedUsername, 3600);
-                System.out.println("Đăng nhập thành công");
-	            } else {
-	                System.out.println("Không tìm thấy tài khoản");
-	          }
+			if (account != null) {
+				String cleanedUsername = account.getUserName().replaceAll("\\s", "");
+				cookieService.setCookie(response, "username", cleanedUsername, 3600);
+				System.out.println("Đăng nhập thành công");
+			} else {
+				System.out.println("Không tìm thấy tài khoản");
+			}
 
-           return "redirect:/client/index";
-      } else {
-          return "redirect:/client/index";
-       }
+			return "redirect:/client/index";
+		} else {
+			return "redirect:/client/index";
+		}
 	}
-	  @RequestMapping(value = "/client/signin/error")
-	   public String loi (Model model) {
-		  model.addAttribute("loi", "Sai thông tin đăng nhập, Vui lòng nhập lại");
-		  return "login";
-	  }
-	  
-	  @RequestMapping(value = "/client/social/success")
-	   public String loginGG (Model model) {
-		 
-		  return "redirect:/client/index";
-	  }
+
+	@RequestMapping(value = "/client/signin/error")
+	public String loi(Model model) {
+		model.addAttribute("loi", "Sai thông tin đăng nhập, Vui lòng nhập lại");
+		return "login";
+	}
+
+	@RequestMapping(value = "/client/social/success")
+	public String loginGG(Model model) {
+
+		return "redirect:/client/index";
+	}
 }
