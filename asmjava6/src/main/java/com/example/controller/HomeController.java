75945cabfae237875b6c6a3f084ab76887e74cab
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-08-08 10:34:29 +0700

    123

--- asmjava6/src/main/java/com/example/controller/HomeController.java
+++ asmjava6/src/main/java/com/example/controller/HomeController.java
@@ -1,24 +1,20 @@
 package com.example.controller;
 
-import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.entity.Account;
 import com.example.entity.Cart;
 import com.example.entity.Product;
-import com.example.entity.Account;
 import com.example.jparepository.ProductRepository;
 import com.example.service.CartService;
-import com.example.service.impl.CartServiceImpl;
 
 @Controller
 public class HomeController {
@@ -76,6 +72,13 @@ public class HomeController {
 			model.addAttribute("products", page);
 		}
 		return "index";
+	}
+
+	@RequestMapping("/cart")
+	public String cart(Model model) {
+		List<Cart> list = cartService.findAll();
+		model.addAttribute("carts", list);
+		return "cart";
 	}
 
 }
